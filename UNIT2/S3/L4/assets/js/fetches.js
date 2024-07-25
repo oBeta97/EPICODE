a2ed43fe77f02723d26a49f8b20f147715f33419
oBeta97 <136509239+oBeta97@users.noreply.github.com>
2024-07-25 17:26:43 +0200

    modifiche esercizio

--- UNIT2/S3/L4/assets/js/fetches.js
+++ UNIT2/S3/L4/assets/js/fetches.js
@@ -27,3 +27,34 @@ async function GetImages(searchWord){
     });
 
 }
+
+
+async function GetImage(imageId){
+
+    const pexelConnectionString = `https://api.pexels.com/v1/photos/${imageId}`;
+
+
+    return await fetch(pexelConnectionString,
+        {
+            method: 'GET',
+            headers: {
+                Authorization: "O2z8Lxm4xNKq9BmI1gTsM9F1PBCAjPl2tQ5nwUyYT2DmiOtRdkk2gV2l"
+            }
+        }
+    ).
+    then(function(promise){
+        if(!promise.ok)
+            throw new Error('Bad response');
+
+        return promise.json();
+    }).
+    then(function(images){
+        return images;
+    }).
+    catch(function(err){
+        alert('fetch didn\'t work')
+        console.log(err);
+        return null;
+    });
+
+}
